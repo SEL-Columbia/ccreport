dd6e6d0e82bb589549ecb6c6984429358dbaada6
Ukang'a Dickson <ukanga@gmail.com>
2012-09-11 13:46:07 +0300

    use CommcareReport

--- report/views.py
+++ report/views.py
@@ -1,24 +1,19 @@
 # encoding=utf-8
 # maintainer: katembu
 
-from django import forms
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required, permission_required
-from django.shortcuts import redirect
-from django.conf import settings
-from django.views.decorators.csrf import csrf_protect
-from django.http import HttpResponse, Http404
-from django.utils.translation import ugettext_lazy as _, ugettext
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from django.utils.translation import ugettext as _
 
-from report.models import CommcareReportURL
+from report.models import CommcareReport
 
 @login_required()
 def index(request):
     '''Landing page '''
-    context = {'title': _(u"Report Dashboard")}
-    
-    context.update({'report': CommcareReportURL.objects.all()});
-    
+    context = RequestContext(request)
+    context.commcare_reports = CommcareReport.objects.all()
+    context.title = _(u"Report Dashboard")
     return render(request, "home.html", context)
     
 
