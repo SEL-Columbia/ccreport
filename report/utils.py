2b532593600400001bfac7ef0ec473eb5f04dcd2
Ukang'a Dickson <ukanga@gmail.com>
2012-09-11 13:36:48 +0300

    utility functions to get data from commcarehq

--- report/utils.py
+++ report/utils.py
@@ -1,7 +1,12 @@
 # encoding=utf-8
+import StringIO
+import calendar
 
 import datetime
 import json
+import zipfile
+import httplib2
+import os
 
 
 class DownloadFailed(StandardError):
@@ -33,4 +38,41 @@ def get_option(report):
     try:
         return CommcareReportURL.objects.get(pk=report.pk).dataset_id
     except:
-        raise ProjectUnconfigured(project)
+        raise ProjectUnconfigured(report)
+
+
+def get_data_from_url(url, username='', password=''):
+    """Fetches data from the given url, if username is supplied it will apply
+credentials with the given username and password.
+
+The return value is a tuple of (response, content), the first
+being and instance of the 'Response' class, the second being
+a string that contains the response entity body.
+    """
+    http = httplib2.Http()
+    if username != '':
+        http.add_credentials(username, password)
+    response, content = http.request(url)
+    return response, content
+
+
+def get_timestamp():
+    """Returns unix timestamp"""
+    d = datetime.datetime.now()
+    return calendar.timegm(d.utctimetuple())
+
+
+def download_commcare_zip_report(url, username, password):
+    """Downloads commcares csv exoorts given a commcare csv export url"""
+    ZIPDIR = "output"
+    response, content = get_data_from_url(url, username, password)
+    if response.status == 200 and\
+       response['content-type'] == 'application/zip':
+        zip_data = StringIO.StringIO(content)
+        zf = zipfile.ZipFile(zip_data)
+        path = os.path.join(ZIPDIR, '%s' % get_timestamp())
+        if not os.path.exists(path):
+            os.mkdir(path)
+        zf.extractall(path)
+        return os.path.join(path, '#.csv')
+    return None
