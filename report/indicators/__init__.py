d31c6b89fc5627b4de4ad0d60327574d9d23f743
ukanga <ukanga@gmail.com>
2012-09-20 15:17:18 +0300

    function generates an indicator list

--- report/indicators/__init__.py
+++ report/indicators/__init__.py
@@ -1,1 +1,17 @@
-from report.indicators.MalariaIndicator import MalariaIndicator  
+import inspect
+import sys
+from report.indicators.MalariaIndicator import MalariaIndicator
+
+
+def get_indicator_list():
+    """return a list of dict with Indicator info"""
+    _ind_list = []
+    for name, obj in inspect.getmembers(sys.modules[__name__]):
+        if inspect.isclass(obj):
+            for n, o in inspect.getmembers(obj):
+                print n, o, type(o)
+                if inspect.ismethod(o) and n.endswith('_indicator'):
+                    doc = o.__doc__
+                    _ind = {'class': obj, 'func': o, 'description': doc}
+                    _ind_list.append(_ind)
+    return _ind_list
